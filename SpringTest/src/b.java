4ea6d93e3fc0355b68e0fd387d1ee0bf943cf985
haige6502296 <446836638@qq.com>
2019-02-16 03:13:33 +0700

    加了些代码

--- SpringTest/src/b.java
+++ SpringTest/src/b.java
@@ -1,2 +1,19 @@
 public class b {
+  public static void main(String[] args) {
+int n=8;
+    int m=6;
+    System.out.println(n);
+    System.out.println(m);
+  try{
+    int x=0;
+    int y=5;
+    System.out.println(y/x);
+  }catch(Exception e){
+  System.out.println(e);
+  }
+finally {
+    System.out.println("y/x分母为0无意义！！");
+  }
+
+  }
 }

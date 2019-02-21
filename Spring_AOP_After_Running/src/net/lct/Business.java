a97480eed201fe6ff97aece701dcdf639624a20d
haige6502296 <446836638@qq.com>
2019-02-21 19:47:34 +0700

    Spring_AOP_After_Returning

---
+++ Spring_AOP_After_Running/src/net/lct/Business.java
@@ -0,0 +1,13 @@
+package net.lct;
+
+public class Business {
+  int m() {
+    System.out.println("m() 被调用了_______");
+    return 2;
+  }
+
+  int n() {
+    System.out.println("k() 方法被调用了________");
+    return 3;
+  }
+}
